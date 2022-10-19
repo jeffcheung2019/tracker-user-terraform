0e855dd7b76e0658c6c5c5bfd0fd1c09d5eedf8a
Cheung jeff <cheungjeff@Cheungs-MacBook-Pro.local>
2022-10-19 14:44:43 +0800

    1st commit

---
+++ versions.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_version = ">= 0.13.1"
+
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = ">= 3.73"
+    }
+  }
+  
+  backend "remote" {
+    organization = "jeffcheung2019"
+    workspaces {
+      name = "tracker_user_terraform"
+    }
+  }
+}
+
+provider "aws" {
+  region = "us-west-1"
+
+  access_key = "${var.aws_access_key}"
+  secret_key = "${var.aws_secret_key}"
+}
